57f997e0b141e7f234b12cf3bbfdf189f7d0b97c
Sílvio Ricardo <63252393+silviocastro006@users.noreply.github.com>
2025-06-27 17:41:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: "/jhams-academy-mock/", // <- ESSENCIAL para GitHub Pages
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
